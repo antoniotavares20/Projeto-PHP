d8dbe70831ab3033ffc16512f8f9843611c012ff
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-08-12 20:26:50 -0300

    testando classe abstrata

---
+++ abstrat/exercicio1.php
@@ -0,0 +1,33 @@
+﻿<?php
+	interface Veiculo{
+		public function acelerar($velocidade);
+		public function frenar($frenar);
+		public function trocarMacha($marcha);
+	}
+
+	/*
+	 *caso queira implementar outra interface é necessário usar a ','
+	 *A diferenca de classe e interface é que o metodo abstrato so pode ser implementada uma vez 
+	 */
+	abstract class Automovel implements Veiculo{
+		public function acelerar($velocidade){
+			echo "O Veiculo acelerou até  ". $velocidade . "Km/h";
+		}
+		public function frenar($frenar){
+			echo "O Veiculo freiou ". $frenar . "Km/h" ;
+		}
+		public function trocarMacha($marcha){
+			echo "O Veiculo trocou a macha". $trocarMarcha . "Km/h";
+		}
+	}
+
+	class DelRey extends Automovel{
+		public function empurrar(){
+
+		}
+	}
+	
+	$carro = new DelRey();
+	$carro->acelerar(200);
+
+?>
